9f9333dd78d66aba474e54e2df73763ffde84a02
yourlx <114220266+yourlx@users.noreply.github.com>
2024-07-02 21:50:17 +0700

    Fix product update, examples

--- src/HardwareStore.WebApi/Dto/ImageDto.cs
+++ src/HardwareStore.WebApi/Dto/ImageDto.cs
@@ -4,5 +4,5 @@
 /// Image DTO.
 /// </summary>
 /// <param name="Id" example="3fa85f64-5717-4562-b3fc-2c963f66afa6">Image ID.</param>
-/// <param name="Content">Image content in byte array format.</param>
+/// <param name="Content" example="here should be base64 string">Image content in byte array format.</param>
 public record ImageDto(Guid Id, byte[] Content);
--- src/HardwareStore.WebApi/Dto/ProductDto.cs
+++ src/HardwareStore.WebApi/Dto/ProductDto.cs
@@ -8,9 +8,9 @@
 /// <param name="Category" example="Electronics">Product category.</param>
 /// <param name="Price" example="999.99">Product price.</param>
 /// <param name="AvailableStock" example="50">Available stock of the product.</param>
-/// <param name="LastUpdate" example="2024-07-02T14:30:00Z">Last update date and time of the product.</param>
+/// <param name="LastUpdate" example="2024-07-02T14:30:00.00Z">Last update date and time of the product.</param>
 /// <param name="Supplier">Supplier information.</param>
-/// <param name="Image">Image information.</param>
+/// <param name="ImageId" example="3fa85f64-5717-4562-b3fc-2c963f66afa6">Image id.</param>
 public record ProductDto(
     Guid Id,
     string Name,
@@ -19,4 +19,4 @@ public record ProductDto(
     int AvailableStock,
     DateTime LastUpdate,
     SupplierDto Supplier,
-    ImageDto? Image);
+    Guid ImageId);
